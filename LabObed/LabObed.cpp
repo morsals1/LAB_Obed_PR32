51eeb0f2a3fa0a0a951534371809fe9dc955568d
morsals1 <157448960+morsals1@users.noreply.github.com>
2025-03-03 13:55:29 +0500

    Add files via upload

--- LabObed/LabObed.cpp
+++ LabObed/LabObed.cpp
@@ -1,47 +1,84 @@
-﻿// LabObed.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstring>
 
-#include <iostream>
-#include <vector>
+using namespace std;
 
-typedef struct {
-    char number[12];
-    int type;
-    union {
-        char fio[20];
-        struct 
-        {
-            char name_inst[2];
-            char name_unit[2];
-        }work;
-    } info; 
-} pig;
-pig f;
+struct PhoneEntry {
+    char type[20];
+    char number[15];
+    union Details {
+        struct Personal {
+            char name[50];
+        } personal;
+        struct Service {
+            char institution[50];
+            char department[50];
+        } service;
+    } details;
+};
 
-int main()
-{
-
-    pig f[] = {
-        {"89002118176", 0, {"Kolya Merobov"}},
-        {"89006571122", 1, },
-        {"89016669977", 1, {""}}
-    };
-
-    for (int i = 0; i < 3; ++i) {
-        std::cout << f[i].info.fio << "\n";
+void poisk(const char* institution, PhoneEntry spisok[], int size, PhoneEntry results[], int& resultCount) {
+    resultCount = 0;
+    for (int i = 0; i < size; i++) {
+        if (strcmp(spisok[i].type, "Служебный") == 0 &&
+            strcmp(spisok[i].details.service.institution, institution) == 0) {
+            results[resultCount++] = spisok[i];
+        }
     }
-
-
-    std::cout << "\n";
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void bubbleSort(PhoneEntry arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (strcmp(arr[j].number, arr[j + 1].number) > 0) {
+                PhoneEntry temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    PhoneEntry entries[5];
+
+    strcpy_s(entries[0].type, "Личный");
+    strcpy_s(entries[0].number, "123-45-67");
+    strcpy_s(entries[0].details.personal.name, "Иванов Иван Иванович");
+
+    strcpy_s(entries[1].type, "Служебный");
+    strcpy_s(entries[1].number, "555-01-02");
+    strcpy_s(entries[1].details.service.institution, "Больница №1");
+    strcpy_s(entries[1].details.service.department, "Приемный покой");
+
+    strcpy_s(entries[2].type, "Служебный");
+    strcpy_s(entries[2].number, "555-03-04");
+    strcpy_s(entries[2].details.service.institution, "Поликлиника №3");
+    strcpy_s(entries[2].details.service.department, "Терапия");
+
+    strcpy_s(entries[3].type, "Служебный");
+    strcpy_s(entries[3].number, "555-05-06");
+    strcpy_s(entries[3].details.service.institution, "Больница №1");
+    strcpy_s(entries[3].details.service.department, "Хирургия");
+
+    const int count = 5;
+    char needInstitution[50] = "Больница №1";
+
+    PhoneEntry needs[5];
+    int foundCount = 0;
+
+    poisk(needInstitution, entries, count, needs, foundCount);
+
+    bubbleSort(needs, foundCount);
+
+    cout << "Телефоны учреждения '" << needInstitution << "':" << endl;
+    for (int i = 0; i < foundCount; i++) {
+        cout << "Номер: " << needs[i].number
+            << ", Учреждение: " << needs[i].details.service.institution
+            << ", Подразделение: " << needs[i].details.service.department << endl;
+    }
+
+    return 0;
+}
